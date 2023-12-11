ca428c0ac31c7c1314562b7ced7c13a15491eb8d
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-11 10:40:55 +0100

    added the "recording" endpoint

---
+++ RestAPI/WebAPI/Controllers/RecordingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/recording")]
+	[ApiController]
+	public class RecordingController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Hello World!");
+		}
+	}
+}
