d0e50709a1bf6f8304a0cf85805d014d44a21426
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 18:18:06 +0100

    removed the old swagger

--- RestAPI/WebAPI/Program.cs
+++ RestAPI/WebAPI/Program.cs
@@ -13,9 +13,6 @@ namespace WebAPI
 			// Add services to the container.
 
 			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			//builder.Services.AddEndpointsApiExplorer();
-			//builder.Services.AddSwaggerGen();
 
 			builder.Services.AddOpenApiDocument(configure =>
 			{
