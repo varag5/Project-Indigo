b1b379836420dc1e680ca3660b5eb6145f33ce06
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 16:32:38 +0100

    RecordingController skeleton done

--- RestAPI/WebAPI/Controllers/RecordingController.cs
+++ RestAPI/WebAPI/Controllers/RecordingController.cs
@@ -7,8 +7,36 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class RecordingController : ControllerBase
 	{
+		//GET: api/recording
+		/// <summary>
+		/// Gets all recordings.
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet]
-		public IActionResult Get()
+		public IActionResult GetRecordings()
+		{
+			return Ok("Hello World!");
+		}
+
+		//GET: api/recording/{id}
+		/// <summary>
+		/// Gets the specified recording.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpGet("{id}")]
+		public IActionResult GetRecording(int id)
+		{
+			return Ok($"Hello! Recording is {id}");
+		}
+
+		// POST: api/recording
+		/// <summary>
+		/// Creates a new recording.
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public IActionResult CreateNewRecording()
 		{
 			return Ok("Hello World!");
 		}
