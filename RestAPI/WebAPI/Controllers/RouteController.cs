11f031cb5dca158b88e998cf67f85db12ae31160
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 12:39:55 +0100

    API Controllers added

---
+++ RestAPI/WebAPI/Controllers/RouteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/route")]
+	[ApiController]
+	public class RouteController : ControllerBase
+	{
+	}
+}
