9283f69566b8c76b46ed08d235a6d9755a67c536
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 16:27:01 +0100

    Itinerary skeleton done

--- RestAPI/WebAPI/Controllers/ItineraryController.cs
+++ RestAPI/WebAPI/Controllers/ItineraryController.cs
@@ -7,5 +7,52 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class ItineraryController : ControllerBase
 	{
+		// POST: api/itinerary
+		/// <summary>
+		/// Creates a new itinerary.
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public IActionResult CreateNewItinerary()
+		{
+			return Ok("Hello World!");
+		}
+
+		//PUT: api/itinerary/{id}
+		/// <summary>
+		/// Modify an existing itinerary.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[HttpPut("{id}")]
+		public IActionResult UpdateItinerary(int id)
+		{
+			return Ok("Hello World!");
+		}
+
+		//GET: api/itinerary/start={start}&end={end}
+		/// <summary>
+		/// Gets an itinerary between two locations.
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <returns></returns>
+		[HttpGet]
+		public IActionResult GetItinerary(string start = null, string end = null)
+		{
+			return Ok($"Hello! Start is {start} end is {end}");
+		}
+
+		//GET: api/itinerary/user={userId}
+		/// <summary>
+		/// Gets all itineraries for a user.
+		/// </summary>
+		/// <param name="userId"></param>
+		/// <returns></returns>
+		[HttpGet("{userId}")]
+		public IActionResult GetItinerary(int userId)
+		{
+			return Ok($"Hello! User is {userId}");
+		}
 	}
 }
