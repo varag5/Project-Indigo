11b5c106ae61b2f069e754b992ffb3888573c577
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 12:58:26 +0100

    route controller skeleton

--- RestAPI/WebAPI/Controllers/RouteController.cs
+++ RestAPI/WebAPI/Controllers/RouteController.cs
@@ -7,5 +7,27 @@ namespace WebAPI.Controllers
 	[ApiController]
 	public class RouteController : ControllerBase
 	{
+		// POST: api/route
+		/// <summary>
+		/// Creates a new route.
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public IActionResult CreateNewRoute()
+		{
+			return Ok("Hello World!");
+		}
+
+		//PUT: api/route/{id}
+		/// <summary>
+		/// Updates the specified route.
+		/// </summary>
+		/// <param name="id">The original id of the route</param>
+		/// <returns></returns>
+		[HttpPut("{id}")]
+		public IActionResult UpdateRoute(int id)
+		{
+			return Ok("Hello World!");
+		}
 	}
 }
