167e373332a196ae0c4239ab9a7d708ebc62d29b
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-16 18:12:07 +0100

    NSwag documentation added

--- RestAPI/WebAPI/Program.cs
+++ RestAPI/WebAPI/Program.cs
@@ -14,16 +14,23 @@ namespace WebAPI
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+			//builder.Services.AddEndpointsApiExplorer();
+			//builder.Services.AddSwaggerGen();
+
+			builder.Services.AddOpenApiDocument(configure =>
+			{
+				configure.Title = "Indigo API";
+				configure.Version = "v1";
+				configure.Description = "An ASP.NET Web API for the Indigo Project";
+			});
 
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
+				app.UseOpenApi();
+				app.UseSwaggerUi3();
 			}
 
 			app.UseHttpsRedirection();
