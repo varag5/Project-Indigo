e0922ba743c8c769f9e66ff3635318c4a45605a2
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-23 13:40:22 +0100

    modification

--- RestAPI/Bll/Services/Mock/ItineraryService.cs
+++ RestAPI/Bll/Services/Mock/ItineraryService.cs
@@ -7,22 +7,22 @@ namespace Bll.Services.Mock
 {
 	public class ItineraryService : IItineraryService
 	{
-		public void CreateNewItinerary(ItineraryDto itinerary)
+		public ItineraryDto CreateNewItinerary(ItineraryDto itinerary)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void GetItinerary(int userId)
+		public ItineraryDto GetItinerary(int userId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void GetItinerary(string start, string end)
+		public ItineraryDto GetItinerary(string start, string end)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void UpdateItinerary(int id, ItineraryDto itinerary)
+		public ItineraryDto UpdateItinerary(int id, ItineraryDto itinerary)
 		{
 			throw new NotImplementedException();
 		}
