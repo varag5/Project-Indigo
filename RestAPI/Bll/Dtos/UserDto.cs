be314e7917cb9c8086fddc68f2ec3816efdddf97
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-28 21:21:25 +0100

    start populating the DTOs

--- RestAPI/Bll/Dtos/UserDto.cs
+++ RestAPI/Bll/Dtos/UserDto.cs
@@ -6,5 +6,8 @@ namespace Bll.Dtos
 {
 	public class UserDto
 	{
-	}
+        public long ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
