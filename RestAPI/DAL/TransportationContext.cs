3d30386bdd4476ae22368616bef9985adc001efd
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 09:52:40 +0100

    Add the tables to the context

--- RestAPI/DAL/TransportationContext.cs
+++ RestAPI/DAL/TransportationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,16 @@ namespace DAL
 {
 	public class TransportationContext : DbContext
 	{
-		public TransportationContext(DbContextOptions<TransportationContext> options) : base(options) { }
+		public DbSet<Route> Routes { get; set; }
+		public DbSet<Stop> Stops { get; set; }
+		public DbSet<BoundedWalk> BoundedWalks { get; set; }
+		public DbSet<Walk> Walks { get; set; }
+		public DbSet<Itinerary> Itineraries { get; set; }
+        public DbSet<Recording> Recordings { get; set; }
+        public DbSet<RouteJourney> RouteJourneys { get; set; }
+        public DbSet<RouteStop> RouteStops { get; set; }
+
+        public TransportationContext(DbContextOptions<TransportationContext> options) : base(options) { }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
