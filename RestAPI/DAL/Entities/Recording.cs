443f8a1a88d7a516c5a1697ad07000ff74a62a8e
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 11:16:05 +0100

    Recording populated

--- RestAPI/DAL/Entities/Recording.cs
+++ RestAPI/DAL/Entities/Recording.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace DAL.Entities
@@ -7,5 +8,12 @@ namespace DAL.Entities
 	public class Recording
 	{
         public int ID { get; set; }
-	}
+        [Required]
+        public int Order { get; set; }
+        public RecordingType RecordingType { get; set; }
+
+        [Required]
+        public Itinerary Itinerary { get; set; }
+        public Route? Route { get; set; }
+    }
 }
