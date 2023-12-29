97c05ea6644639a7d28835a3bd59c97e61e7c88c
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 11:09:09 +0100

    RouteStop populated

--- RestAPI/DAL/Entities/RouteStop.cs
+++ RestAPI/DAL/Entities/RouteStop.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace DAL.Entities
@@ -7,5 +8,9 @@ namespace DAL.Entities
 	public class RouteStop
 	{
         public int ID { get; set; }
-	}
+		[Required]
+		public Direction Direction { get; set; }
+		[Required]
+		public int Order { get; set; }
+    }
 }
