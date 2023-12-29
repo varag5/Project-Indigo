6aa93c6a98212599fad774ff65238fdf32c176de
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 11:11:12 +0100

    Itinerary populated

--- RestAPI/DAL/Entities/Itinerary.cs
+++ RestAPI/DAL/Entities/Itinerary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace DAL.Entities
@@ -7,5 +8,9 @@ namespace DAL.Entities
 	public class Itinerary
 	{
         public int ID { get; set; }
-	}
+        [Required]
+        public string Start { get; set; }
+        [Required]
+        public string End { get; set; }
+    }
 }
