082d66e40eed127d61556a48e97906eef2cc4637
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 11:06:06 +0100

    Stop table populated

--- RestAPI/DAL/Entities/Stop.cs
+++ RestAPI/DAL/Entities/Stop.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace DAL.Entities
@@ -7,5 +8,7 @@ namespace DAL.Entities
 	public class Stop
 	{
         public int ID { get; set; }
-	}
+        [Required]
+        public string Name { get; set; }
+    }
 }
