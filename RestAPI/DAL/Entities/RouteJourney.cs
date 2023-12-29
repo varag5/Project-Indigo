c894367b2d433c9c3bc53603b1b61d4bfe40723f
Botond Lajos Perényi <halkszavu@outlook.com>
2023-12-29 11:12:35 +0100

    RouteJourney populated

--- RestAPI/DAL/Entities/RouteJourney.cs
+++ RestAPI/DAL/Entities/RouteJourney.cs
@@ -7,5 +7,10 @@ namespace DAL.Entities
 	public class RouteJourney
 	{
         public int ID { get; set; }
-	}
+
+        public Itinerary Itinerary { get; set; }
+        public Stop Start { get; set; }
+        public Stop End { get; set; }
+        public Route Route { get; set; }
+    }
 }
