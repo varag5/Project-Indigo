01a8526393d1d13d2fe6945a4bfca33cde66e8d8
varag5 <102171347+varag5@users.noreply.github.com>
2024-01-07 22:42:42 +0000

    DataMapping-4 entities

--- RestAPI/Bll/DataMapping/DataMapping.cs
+++ RestAPI/Bll/DataMapping/DataMapping.cs
@@ -11,8 +11,27 @@ public static class DataMapper
 {
     public static ItineraryDto ConvertToDTO(this Itinerary itinerary)
     {
-        return new ItineraryDto { ID = itinerary.ID, Start = itinerary.Start, End = itinerary.End};
+        return new ItineraryDto { ID = itinerary.ID, Start = itinerary.Start, End = itinerary.End };
     }
+
+    public static RecordingDto ConvertToDTO(this Recording recording)
+    {
+        return new RecordingDto { ID = recording.ID, Itinerary = recording.Itinerary.ConvertToDTO() };
+    }
+
+    public static RouteDto ConvertToDTO(this Route route)
+    {
+        return new RouteDto { ID = route.ID, RouteNumber= route.RouteNumber,Provider=route.Provider};
+    }
+
+
+    //public static UserDto ConvertToDTO(this User user)
+    //{
+        //return new UserDto { ID = user.ID, Username = user.Username, Password = user.Password };
+    //}
+    //User isn't added to Entities yet
+
+
 
 }
 
